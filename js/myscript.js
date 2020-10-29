bc4717a36c315e670cd526a048395537a96f79e1
MatthewJ0405 <mattchu0405@rocketmail.com>
2020-10-29 16:55:57 -0400

    Final commit

--- js/myscript.js
+++ js/myscript.js
@@ -1,8 +1,11 @@
 function openCreate() {
     var x = document.getElementById("blogForm");
-    if (x.style.display === "block") {
+    var y = document.getElementById("formDiv");
+    if (y.style.display === "block" && x.style.display === "block") {
+        y.style.display = "none";
         x.style.display = "none";
     } else {
+        y.style.display = "block";
         x.style.display = "block";
     }
 }
@@ -13,10 +16,10 @@ function newPost() {
 
     var titleText = document.createTextNode(postTitle);
     var bodyText = document.createTextNode(postBody);
-    
+
     var postDiv = document.createElement('div');
     postDiv.setAttribute('class', 'col-md-8');
-    
+
     var hr = document.createElement('hr');
 
     var h2 = document.createElement('h2');
@@ -35,4 +38,67 @@ function newPost() {
         document.getElementById('postTitle').value = '';
         document.getElementById('postBody').value = '';
     }
+}
+
+function printError(element, msg) {
+    document.getElementById(element).innerHTML = msg;
+}
+
+function formValidation() {
+    var name = document.contactForm.name.value;
+    var email = document.contactForm.email.value;
+    var phone = document.contactForm.phone.value;
+    var message = document.contactForm.message.value;
+
+    var nameErr = emailErr = phoneErr = messageErr = true;
+
+    if (name == '') {
+        printError('nameErr', 'Please enter your name');
+    } else {
+        var regex = /^[a-zA-z\s]+$/;
+        if (regex.test(name) === false) {
+            printError('nameErr', 'Please enter a valid name');
+        } else {
+            printError('nameErr', '');
+            nameErr = false;
+        }
+    }
+
+    if (email == '') {
+        printError('emailErr', 'Please enter your email address');
+    } else {
+        var regex = /^\S+@\S+\.\S+$/;
+        if (regex.test(email) === false) {
+            printError('emailErr', 'Please enter a valid email address');
+        } else {
+            printError('emailErr', '');
+            emailErr = false;
+        }
+    }
+
+    if (phone == '') {
+        printError('phoneErr', 'Please enter your phone number');
+    } else {
+        var regex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/;
+        if (regex.test(phone) === false) {
+            printError('phoneErr', 'Please enter a valid phone number');
+        } else {
+            printError('phoneErr', '');
+            phoneErr = false;
+        }
+    }
+
+    if (message == '') {
+        printError('messageErr', 'Please enter a message');
+    } else {
+        printError('messageErr', '');
+        messageErr = false;
+    }
+
+    if (nameErr == true || emailErr == true || phoneErr == true || messageErr == true) {
+        return false;
+    } else {
+        alert('You have submitted the form');
+    }
+
 }
